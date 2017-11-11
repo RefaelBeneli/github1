8889391ae1345d7474a95c04a25d5171a2dcec23
Refael Beneli <33573779+RefaelBeneli@users.noreply.github.com>
2017-11-11 17:21:38 +0200

    commit
    commit

---
+++ g.js
@@ -0,0 +1,9 @@
+var server = require('http');
+
+server.createServer(engine).listen(1337);
+
+function engine(request, response)
+{
+  response.writeHead(200,{'content-Type': 'text/plain'});
+  response.end('Hi oleg, this is my first server.');
+}
